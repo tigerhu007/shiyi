2e865f46a9d5af120a6a0ead81d330a88eddd09a
unknown <>
2017-05-14 12:26:04 +0800

    app

---
+++ js/zhc-preson.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+//	联系人数
+	$('.zhc-actop h6').html($('.zhc-list li').length)
+	
+//	点击左滑
+	$('.zhc-li-right p').hover(function(){
+		console.log(1)
+		$(this).css('right','0')
+		$(this).children().first().css('display','inline-block')
+		$(this).children('span').css('display','none')
+	},function(){
+		$(this).css('right','-1.6rem')
+		$(this).children().first().css('display','none')
+		$(this).children('span').css('display','inline-block')
+	})
+//		删除
+	$('.zhc-li-right p img').each(function(index,value){
+		value.addEventListener('touchstart',function(){
+			$(this).parentsUntil('ul').remove()	
+		})
+	})
+		
+	$('.zhc-headerbottom a')[0].addEventListener('touchstart',function(){
+		window.history.back()
+	})
+	
+})
